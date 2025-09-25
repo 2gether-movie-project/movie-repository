0b91e77af42327e3d5eac0f57d40fe0e6949ac40
Park-yh <pyh5421@naver.com>
2025-09-25 14:52:11 +0900

    feat(movie): 영화 ID 조회 및 예외 처리 추가

--- src/main/java/com/movieproject/domain/movie/service/internal/MovieInternalService.java
+++ src/main/java/com/movieproject/domain/movie/service/internal/MovieInternalService.java
@@ -1,10 +1,11 @@
 package com.movieproject.domain.movie.service.internal;
 
+import com.movieproject.common.exception.GlobalException;
 import com.movieproject.domain.director.entity.Director;
 import com.movieproject.domain.director.service.DirectorExternalService;
-import com.movieproject.domain.director.service.DirectorInternalService;
 import com.movieproject.domain.movie.dto.MovieRequestDto;
 import com.movieproject.domain.movie.entity.Movie;
+import com.movieproject.domain.movie.exception.MovieErrorCode;
 import com.movieproject.domain.movie.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class MovieInternalService {
 
     private final MovieRepository movieRepository;
     private final DirectorExternalService directorExternalService;
 
+    @Transactional
     public Movie createMovie(MovieRequestDto.Create requestDto) {
 
         Director director = directorExternalService.findDirectorById(requestDto.directorId());
@@ -32,5 +34,16 @@ public class MovieInternalService {
                 .build();
 
         return movieRepository.save(movie);
+    }
+
+    /**
+     * ID로 영화 엔티티를 조회하는 내부 메서드
+     * @param movieId 조회할 영화의 ID
+     * @return 찾아낸 Movie 엔티티
+     * @throws GlobalException 해당 ID의 영화가 없을 경우 발생
+     */
+    public Movie getMovieByMovieId(Long movieId) {
+        return movieRepository.findById(movieId)
+                .orElseThrow(() -> new GlobalException(MovieErrorCode.MOVIE_NOT_FOUND));
     }
 }
