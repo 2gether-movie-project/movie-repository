5157193477d2a836292279301ecc92ccd1b8063d
moominIsCute <1happymoomin1@gmail.com>
2025-09-26 10:54:25 +0900

    feat:userinreviewresponsedto 추가

--- src/main/java/com/movieproject/domain/user/dto/response/UserInReviewResponseDto.java
+++ src/main/java/com/movieproject/domain/user/dto/response/UserInReviewResponseDto.java
@@ -1,10 +1,16 @@
 package com.movieproject.domain.user.dto.response;
 
+import com.movieproject.domain.user.entity.User;
+
 public record UserInReviewResponseDto(
         Long userId,
         String username
 ) {
     public static UserInReviewResponseDto of(Long userId, String username) {
         return new UserInReviewResponseDto(userId, username);
+    }
+
+    public static UserInReviewResponseDto from(User user) {
+        return new UserInReviewResponseDto(user.getUserId(), user.getUsername());
     }
 }
